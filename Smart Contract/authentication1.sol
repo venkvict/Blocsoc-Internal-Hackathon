40c02932021167d86c171809b56f1c294f5eb0a1
venkvict <147480510+venkvict@users.noreply.github.com>
2024-06-28 16:55:11 +0530

    Update authentication1.sol

--- Smart Contract/authentication1.sol
+++ Smart Contract/authentication1.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 //authentication for user as well as for company 
 contract Auth {
     struct User {
@@ -8,12 +8,10 @@ contract Auth {
     }
 
     mapping(address => User) private users;
-
     function register(string memory username, string memory password) public {
         require(bytes(users[msg.sender].username).length == 0, "User already exists");
         users[msg.sender] = User(username, keccak256(abi.encodePacked(password)));
     }
-
     function login(string memory username, string memory password) public view returns (bool) {
         if (keccak256(abi.encodePacked(username)) == keccak256(abi.encodePacked(users[msg.sender].username)) &&
             keccak256(abi.encodePacked(password)) == users[msg.sender].passwordHash) {
@@ -21,7 +19,6 @@ contract Auth {
         }
         return false;
     }
-
     function getUsername() public view returns (string memory) {
         require(bytes(users[msg.sender].username).length != 0, "User not found");
         return users[msg.sender].username;
