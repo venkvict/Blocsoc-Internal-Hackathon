e4f9afa0881aea5c4176004e8203bd234c2f852f
venkvict <147480510+venkvict@users.noreply.github.com>
2024-06-28 17:04:51 +0530

    Create nftmint.sol

---
+++ Smart Contract/nftmint.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("MyNFT", "NFT") {
+        tokenCounter = 0;
+    }
+
+    function createNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(recipient, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter++;
+        return newTokenId;
+    }
+}
