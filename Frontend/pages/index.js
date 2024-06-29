ab93e898e96f2793f04a5df013227161505023a2
practicalPanda7325 <156360185+practicalPanda7325@users.noreply.github.com>
2024-06-29 21:42:15 +0530

    Add files via upload

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,60 +1,63 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { useWeb3React } from "@web3-react/core";
-import {InjectedConnector} from "@web3-react/injected-connector";
 import {abi} from "../constants/abi"
-
-const injected = new InjectedConnector(); 
+import { useRef, useState } from "react";
+import { ethers } from "ethers";
 
 export default function Home() {
 
-  const { active, library:provider } = useWeb3React();
+  const [isConnected, setIsConnected] = useState(false);
+  const [signer, setSigner] = useState();
 
-  const signer = provider.getSigner();
-  const contractAddress = "";
-  const contract = new ethers.Contract(contractAddress, abi, signer)
+  const mintpoints= useRef();
+  const merchname = useRef();
 
-  async function connect()  {
-    try{
-      await activate(injected);
-      let accounts = await provider.request({ method: "eth_requestAccounts" });
-      let accountAddress = accounts[0];
-    }catch(e){
-      console.log("In connect function-index.js",e)
-    }
-  }
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  let points,merchName;
+  const [mintForm, setMintForm] = useState(false);
 
-  async function mint() {
-    if(active){
+  async function connect() {
+    if(typeof window.ethereum !== "undefined"){
       try{
-        contract.mintNft()
+        await ethereum.request({method: "eth_requestAccounts"});
+        setIsConnected(true);
+        let connectedProvider = new ethers.providers.Web3Provider(window.ethereum);
+        setSigner(connectedProvider.getSigner)
       }catch(e){
         console.log(e)
       }
-    }
+  }else{
+    setIsConnected(false)
+  }}
+
+  async function handleMintSubmit(){
+    console.log("handleMintSubmit")
+    points = mintpoints.current.value;
+    merchName = merchname.current.value;
+    console.log(points,merchName);
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    console.log(contract)
+    await contract.mintNft(points,merchName);
   }
-  let button = False;
+  
 
   return (
     <div> 
-      if(active){
+      {isConnected?(
       <>
         <p>Connected!</p>
-        <button onClick={button = True}>Mint</button>
-        if(button){
-          <form>
-            <label for="mintPoints">Points: </label>
-            <input type="number" id="mintPoints" name="mintPoints"></input>
-            <input></input>
-            <input type="submit" value="Submit"></input>
-          </form>
-        }
+        <button onClick={()=>setMintForm(!mintForm)}>Mint</button>
+        {mintForm && <div>
+          <label htmlFor="mintPoints">Points: </label>
+          <input type="number" id="mintPoints" name="mintPoints" ref={mintpoints} placeholder="Enter Points"></input><br></br>
+          <label htmlFor="mintPoints">Product Name/Description: </label>
+          <input type ="text" id="mintDesc" name="mintDesc" ref={merchname} placeholder="Enter Name"></input><br></br>
+          <button id="mintSubmit" onClick={() => {handleMintSubmit()}}> Submit</button>
+        </div>}
       </>
-      }
-      else{
+      ):(
         <button onClick={() => connect()}>Connect</button>
-      }
+      )
+    }
     </div>
   );
 }
